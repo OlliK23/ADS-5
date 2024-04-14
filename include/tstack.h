f8c105d11f7cf6309cce43d69ee76ca0508458a2
OlliK23 <160269347+OlliK23@users.noreply.github.com>
2024-04-14 14:56:22 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,28 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T arr[size];
+    int top;
+public:
+    TStack() :top(-1) { }
+    T get() {
+        return arr[top];
+    }
+    bool isEmpty() {
+        return top == -1;
+    }
+    bool isFull() {
+        return top == size - 1;
+    }
+    void pop() {
+        if (top >= 0)
+            top--;
+    }
+    void push(T item) {
+        if (top < size - 1)
+            arr[++top] = item;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
