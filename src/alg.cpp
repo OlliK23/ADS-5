fe9202eb3029bffdf317106c9be2b163af4ca6a7
OlliK23 <160269347+OlliK23@users.noreply.github.com>
2024-04-14 23:50:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,6 @@
 // Copyright 2021 NNTU-CS
 #include <string>
+#include <cctype>
 #include <map>
 #include "tstack.h"
 int priority(char ch) {
@@ -17,35 +18,30 @@ bool isOperator(char x) {
 }
 
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-      std::string postfix;
+    std::string postfix;
     Stack<char, 100> stack;
-    for (int i = 0; i < infix.length(); i++) {
-        if (isdigit(infix[i])) {
-            postfix += infix[i];
-        }
-        else if (isOperator(infix[i])) {
-            if (infix[i] == '(') {
-                stack.push(infix[i]);
-            }
-            else if (stack.isEmpty() || stack.get() == '(' || priority(infix[i]) > priority(stack.get())) {
-                stack.push(infix[i]);
-            }
-            else if (infix[i] == ')') {
+    for (int i = 0; i < inf.length(); i++) {
+        if (isdigit(inf[i])) {
+            postfix += inf[i];
+        }else if (isOperator(inf[i])) {
+            if (inf[i] == '(') {
+                stack.push(inf[i]);
+            }else if (stack.isEmpty() || stack.get() == '(' || priority(inf[i]) > priority(stack.get())) {
+                stack.push(inf[i]);
+            }else if (inf[i] == ')') {
                 while (!stack.isEmpty() && stack.get() != '(') {
                     postfix += ' ';
                     postfix += stack.get();
                     stack.pop();
                 }
                 stack.pop();
-            }
-            else {
-                while (!stack.isEmpty() && priority(infix[i]) <= priority(stack.get())) {
+            }else {
+                while (!stack.isEmpty() && priority(inf[i]) <= priority(stack.get())) {
                     postfix += ' ';
                     postfix += stack.get();
                     stack.pop();
                 }
-                stack.push(infix[i]);
+                stack.push(inf[i]);
             }
         }
     }
@@ -57,45 +53,40 @@ std::string infx2pstfx(std::string inf) {
     return postfix;
 }
 
+
 int eval(std::string pref) {
-  // добавьте код
-Stack<int, 100> stack;
-std::string number = "";
-for (int i = 0; i < pref.length(); i++) {
-    if (pref[i] == ' ') {
-        continue;
+    Stack<int, 100> stack;
+    std::string number = "";
+    for (int i = 0; i < pref.length(); i++) {
+        if (pref[i] == ' ') {
+            continue;
+        }
+        number += pref[i];
     }
-    number += pref[i];
-}
-for (int i = 0; i < number.length(); i++) {
-    int num = number[i];
-    if (isdigit(num)) {
-        stack.push(num -'0');
-    }
-    else if (isOperator(number[i])) {
-        int n1 = stack.get();
-        stack.pop();
-        int n2 = stack.get();
-        stack.pop();
-        if (num == '+') {
-            stack.push(n1 + n2);
-        }
-        else if (num == '-') {
-            stack.push(n2 - n1);
-        }
-        else if (num == '*') {
-            stack.push(n1 * n2);
-        }
-        else if (num == '/') {
-            stack.push(n2 / n1);
+    for (int i = 0; i < number.length(); i++) {
+        int num = number[i];
+        if (isdigit(num)) {
+            stack.push(num -'0');
+        }else if (isOperator(number[i])) {
+            int n1 = stack.get();
+            stack.pop();
+            int n2 = stack.get();
+            stack.pop();
+            if (num == '+') {
+                stack.push(n1 + n2);
+            }else if (num == '-') {
+                stack.push(n2 - n1);
+            }else if (num == '*') {
+                stack.push(n1 * n2);
+            }else if (num == '/') {
+                stack.push(n2 / n1);
+            }else {
+                continue;
+            }
         }
         else {
             continue;
         }
     }
-    else {
-        continue;
-    }
-}
-return stack.get();
+    return stack.get();
 }
