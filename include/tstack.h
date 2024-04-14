d769d5f26e7df69851d938dd469397a8bdd6015a
OlliK23 <160269347+OlliK23@users.noreply.github.com>
2024-04-14 21:28:06 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,30 +2,34 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template <typename T, int size>
 class TStack {
 private:
-    T arr[size];
-    int top;
+	T* arr = nullptr;
+	int top;
 public:
-    TStack() :top(-1) { }
-    T get() {
-        return arr[top];
-    }
-    bool isEmpty() {
-        return top == -1;
-    }
-    bool isFull() {
-        return top == size - 1;
-    }
-    void pop() {
-        if (top >= 0)
-            top--;
-    }
-    void push(T item) {
-        if (top < size - 1)
-            arr[++top] = item;
-    }
-};
+	TStack() : top(-1) {
+		arr = new T[size];
+	}
+	T get() {
+		return arr[top];
+	}
+	void pop() {
+		if (top >= 0) {
+			begin--;
+		}
+	}
+	void push(T s) {
+		if (top < size - 1) {
+			arr[++top] = s;
+		}
 
+	}
+	bool isEmpty() {
+		if (top == -1) {
+			return true;
+		}
+		return false;
+	}
+};
 #endif  // INCLUDE_TSTACK_H_
